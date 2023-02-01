b9e7ac790e233c9144ff8e4ac71bb0a11e09a7d5
Tasya Amalia <tasyaamaliasalsa@gmail.com>
2023-02-01 23:15:53 +0700

    add list product

--- src/fakebackend/axiosData.js
+++ src/fakebackend/axiosData.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
 export const getDataMakeup = async () => {
-  const response = await axios.get('https://makeup.p.rapidapi.com/products.json', {
+  const response = await axios.get(
+    'https://makeup.p.rapidapi.com/products.json',
+    {
       params: {brand: 'maybelline'},
-    headers: {
-      'X-RapidAPI-Key': "6f745fea8fmsh124417c37393be9p18a8c9jsn03627ab6b967",
-      'X-RapidAPI-Host': 'makeup.p.rapidapi.com'
-    }
+      headers: {
+        'X-RapidAPI-Key': '6f745fea8fmsh124417c37393be9p18a8c9jsn03627ab6b967',
+        'X-RapidAPI-Host': 'makeup.p.rapidapi.com',
+      },
+    },
+  );
 
-  }, )
-
-  return response.data
-
+  return response.data;
 };
